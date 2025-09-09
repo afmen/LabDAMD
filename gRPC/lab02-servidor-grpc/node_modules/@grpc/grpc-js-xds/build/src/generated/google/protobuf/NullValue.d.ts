1ab16177c8626c5d9523a79f8f8a1c181de53efb
André Mendes <afmendes@sga.pucminas.br>
2025-09-09 17:46:23 -0300

    Instalacao

---
+++ gRPC/lab02-servidor-grpc/node_modules/@grpc/grpc-js-xds/build/src/generated/google/protobuf/NullValue.d.ts
@@ -0,0 +1,5 @@
+export declare const NullValue: {
+    readonly NULL_VALUE: "NULL_VALUE";
+};
+export type NullValue = 'NULL_VALUE' | 0;
+export type NullValue__Output = typeof NullValue[keyof typeof NullValue];
